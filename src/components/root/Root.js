53afc885fb9c681d2036eb6af64d69fbf246b5ac
NormanEWright <n.wright.online@gmail.com>
2023-10-17 22:15:07 -0500

    Export routes object, format code

--- src/components/root/Root.js
+++ src/components/root/Root.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Outlet, NavLink } from "react-router-dom";
-const ROUTES = {
+
+export const ROUTES = {
   CONTACTS: "/contacts",
   APPOINTMENTS: "/appointments",
 };
+
 function Root() {
   return (
     <>
