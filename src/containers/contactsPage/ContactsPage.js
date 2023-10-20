39d7e1225937d2719bbc01f0fb504506b93d1e50
NormanEWright <n.wright.online@gmail.com>
2023-10-19 19:46:06 -0500

    Initialize local state for name, phone and email

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -4,10 +4,9 @@ import { ContactForm } from "../../components/contactForm/ContactForm";
 import { TileList } from "../../components/tileList/TileList";
 
 export const ContactsPage = ({ contacts, addContact}) => {
-  /*
-  Define state variables for 
-  contact info and duplicate check
-  */
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
