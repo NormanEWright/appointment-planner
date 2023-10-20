7631d3e63aa85255fb3930b916a5f6e669b60e0e
NormanEWright <n.wright.online@gmail.com>
2023-10-19 19:37:20 -0500

    Pass props to the Contacts page

--- src/App.js
+++ src/App.js
@@ -11,9 +11,20 @@ import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage
 import { ContactsPage } from "./containers/contactsPage/ContactsPage";
 
 function App() {
-  const [appointments, setAppointments] = useState([]);
   const [contacts, setContacts] = useState([]);
+  const [appointments, setAppointments] = useState([]);
 
+  const addContact = (name, phone, email) => {
+    setContacts([
+      ...contacts,
+      {
+        name,
+        phone,
+        email,
+      },
+    ]);
+  };
+  
   const addAppointment = (name, contact, date, time) => {
     setAppointments([
       ...appointments,
@@ -26,29 +37,12 @@ function App() {
     ]);
   };
 
-  const addContact = (name, phone, email) => {
-    setContacts([
-      ...contacts,
-      {
-        name,
-        phone,
-        email,
-      },
-    ]);
-  };
-
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Root />}>
         <Route index element={<Navigate to={ROUTES.CONTACTS} replace />} />
-        <Route
-          path={ROUTES.CONTACTS}
-          element={<ContactsPage /> /* Add props to ContactsPage */}
-        />
-        <Route
-          path={ROUTES.APPOINTMENTS}
-          element={<AppointmentsPage /> /* Add props to AppointmentsPage */}
-        />
+        <Route path={ROUTES.CONTACTS} element={<ContactsPage contacts={contacts} addContact={addContact} /> } />
+        <Route path={ROUTES.APPOINTMENTS} element={<AppointmentsPage />} />
       </Route>
     )
   );
