c9883b44be8a4fa1edb3f40b39728a0b0b3ac190
NormanEWright <n.wright.online@gmail.com>
2023-10-20 22:32:21 -0500

    Initialize and track local state variables

--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -4,10 +4,10 @@ import { AppointmentForm } from "../../components/appointmentForm/AppointmentFor
 import { TileList } from "../../components/tileList/TileList";
 
 export const AppointmentsPage = ({ appointments, contacts, addAppointments }) => {
-  /*
-  Define state variables for 
-  appointment info
-  */
+  const [name, setName] = useState("");
+  const [contact, setContact] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
